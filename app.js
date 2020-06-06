a717e5a9f1921d80b187f50577a379a1178e5391
cham_why <53082717+chamwhy@users.noreply.github.com>
2020-06-06 18:16:41 +0900

    test

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
+/* 
+해야할거
+-로그인 시스템 <= 생활코딩 패스포트 공부 (google, twiter, facebook)
+-글쓰기 <= 기능찾아보기
+-게시판 <= 기능 찾아보기
+-탐색 알고리즘 짜기 
+-댓글 시스템 <=다른 프로젝트 참고
+-팔로우 시스템 <=참고
+-rss 시스템 <= 공부
+-프리미엄 기능 만들기 <= 공부
+-결제 기능 만들기 <= 공부
+-구글 애드센스 기능 만들기 <=플젝 참고
+*/
+
+'use strict';
+
 const express = require('express');
 const app = express();
 const join = require('path').join;
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 
-//app.use(express.static(join(__dirname, 'public')));
+app.use(express.static(join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded( {extended : false } ));
 
 app.get('/', (req, res)=>{
-    res.send('g');
+    res.sendFile(fs.readFileSync('/index.html', 'utf8'));
 });
+
+app.post('/create', (req, res)=>{
+    //블로그 창설 api
+    res.redirect('/');
+}); 
+
 
 
 app.listen(PORT, ()=>{
