4f845f44e45e4a66667ba8ee433e47848521e4e1
cham_why <53082717+chamwhy@users.noreply.github.com>
2020-06-09 19:32:26 +0900

    Update users.js

--- app/controller/users.js
+++ app/controller/users.js
@@ -9,12 +9,30 @@ exports.logout = () => {
 
 }
 
-exports.join = () => {
-
+exports.join = (req, res, id) => {
+    User.findOne({id: id}, (user)=>{
+        
+    });
 }
 
 exports.create = () => {
     const user = new User(req.body);
-    user.save();
-    res.render();
+    try{
+        await user.save();
+        req.logIn(user, e => {
+            if (e) req.flash('info', 'Sorry! We are not able to log you in!');
+            res.redirect('/' + user.username);
+        });
+    } catch(e) {
+        console.log(e);
+        const errors = Object.keys(e.errors).map(
+            key => e.errors[key].message
+        );
+        res.render('users/signup', {
+            title: 'sign up',
+            errors,
+            user
+        });
+    }
+    
 }
