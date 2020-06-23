f994f85a67516a65fb90b6f7ad5d200e2df1b013
cham_why <53082717+chamwhy@users.noreply.github.com>
2020-06-23 18:44:49 +0900

    add slider

---
+++ config/routes.js
@@ -0,0 +1,11 @@
+module.exports = (app, passport) => {
+  app.post("/getFollings", (req, res) => {});
+
+  app.get("/login", (req, res) => {});
+
+  //apis
+
+  app.post("/userInfo", (req, res) => {
+    const data = req.body;
+  });
+};
---
+++ public/js/index.js
@@ -0,0 +1,34 @@
+function reset() {
+  for (let i = 0; i < 5; i++) {}
+}
+
+function init() {
+  reset();
+}
+init();
+
+const utils = {};
+
+utils.getUserInfo = () => {
+  //get user info
+  const user = {
+    username: "chamwhy",
+    lv: 4,
+    exp: 69,
+    followings: 34,
+    followers: 52,
+  };
+
+  const expslider = document.querySelector("#expslider");
+  let slider = document.createElement("canvas");
+  const ctx = slider.getContext("2d");
+  ctx.canvas.width = 100;
+  ctx.canvas.height = 20;
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, 500, 20);
+  ctx.fillStyle = "rgb(225, 100, 100)";
+  ctx.fillRect(0, 0, (500 / 100) * user.exp, 20);
+  expslider.appendChild(slider);
+};
+
+utils.getUserInfo();
